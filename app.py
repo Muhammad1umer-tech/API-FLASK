602afdff1681910a5a83afaea1daf9145a296815
Muhammad Umer <121258333+Muhammad1umer-tech@users.noreply.github.com>
2023-09-06 23:41:55 +0500

    eight

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify
-from flask import request
 from flask_cors import CORS 
 import sys
 sys.path.append(r'c:\Users\mumer\Desktop\Anaconda\REST_API')
@@ -11,21 +10,26 @@ CORS(app)
 
 @app.route('/', methods=['GET'])
 def api_my_function():
-    result0 = list(notebook.avg_Estimated_Salary())
-    result1 = list(notebook.avg_age_who_Exited())
-    result2 = list(notebook.avg_age_who_Not_Exited())
-    result3 = list(notebook.avg_Tenure())
+    try:
+        result0 = list(notebook.avg_Estimated_Salary())
+        result1 = list(notebook.avg_age_who_Exited())
+        result2 = list(notebook.avg_age_who_Not_Exited())
+        result3 = list(notebook.avg_Tenure())
 
+        results_dict = {
+            'avg_Estimated_Salary': result0,
+            'avg_age_who_Exited': result1,
+            'avg_age_who_Not_Exited': result2,
+            'avg_Tenure': result3
+        }
 
-    results_dict = {
-        'avg_Estimated_Salary': result0,
-        'avg_age_who_Exited': result1,
-        'avg_age_who_Not_Exited': result2,
-        'avg_Tenure': result3
-    }
+        # Convert the dictionary to JSON using jsonify
+        return jsonify(results_dict)
 
-    # Convert the dictionary to JSON using jsonify
-    return jsonify(results_dict)
+    except Exception as e:
+        # Handle the exception and return an error response
+        error_message = str(e)
+        return jsonify({'error': error_message}), 500  # You can choose an appropriate HTTP status code for the error
 
 if __name__ == '__main__':
     app.run(debug=True)
