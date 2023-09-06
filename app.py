56859b7e5cfcb3c0348bc255b37c843c8e9f72c3
Muhammad Umer <121258333+Muhammad1umer-tech@users.noreply.github.com>
2023-09-06 23:36:55 +0500

    seventh

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, jsonify
 from flask import request
 from flask_cors import CORS 
-# import sys
-# sys.path.append(r'c:\Users\mumer\Desktop\Anaconda\REST_API')
-# import notebook
+import sys
+sys.path.append(r'c:\Users\mumer\Desktop\Anaconda\REST_API')
+import notebook
 
 app = Flask(__name__)
 
@@ -11,17 +11,17 @@ CORS(app)
 
 @app.route('/', methods=['GET'])
 def api_my_function():
-    # result0 = list(notebook.avg_Estimated_Salary())
-    # result1 = list(notebook.avg_age_who_Exited())
-    # result2 = list(notebook.avg_age_who_Not_Exited())
-    # result3 = list(notebook.avg_Tenure())
+    result0 = list(notebook.avg_Estimated_Salary())
+    result1 = list(notebook.avg_age_who_Exited())
+    result2 = list(notebook.avg_age_who_Not_Exited())
+    result3 = list(notebook.avg_Tenure())
 
 
     results_dict = {
-        'avg_Estimated_Salary': 0,
-        'avg_age_who_Exited': 1,
-        'avg_age_who_Not_Exited': 1,
-        'avg_Tenure': 2
+        'avg_Estimated_Salary': result0,
+        'avg_age_who_Exited': result1,
+        'avg_age_who_Not_Exited': result2,
+        'avg_Tenure': result3
     }
 
     # Convert the dictionary to JSON using jsonify
